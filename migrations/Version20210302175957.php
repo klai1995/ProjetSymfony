43c2e7f68ec686d73d00e7e7faf9b1a9830c5b2e
klai1995 <50213314+klai1995@users.noreply.github.com>
2021-03-13 13:00:48 +0100

    Update Version20210302175957.php

--- migrations/Version20210302175957.php
+++ migrations/Version20210302175957.php
@@ -22,7 +22,7 @@ final class Version20210302175957 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE evenement (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, langue VARCHAR(255) NOT NULL, place INT NOT NULL, date DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE publication (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, image VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, prenom VARCHAR(255) NOT NULL, sexe TINYINT(1) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+       
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +30,6 @@ final class Version20210302175957 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE evenement');
         $this->addSql('DROP TABLE publication');
-        $this->addSql('DROP TABLE user');
+      
     }
 }
